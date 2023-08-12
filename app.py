29daa878ec1eccd892b06e801fdfa3fd21c1eb83
PakDanan <danan_p@yahoo.com>
2023-08-12 11:16:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from keybert import KeyBERT
 
-sample_text = """
-    Computer Science is the study of computers and computational systems.
+chosen_model = st.selectbox(
+    'Choose model',
+    ('all-MiniLM-L6-v2', 'paraphrase-multilingual-MiniLM-L12-v2', 'paraphrase-mpnet-base-v2','paraphrase-multilingual-mpnet-base-v2'))
+
+sample_text = 
+    """Computer Science is the study of computers and computational systems.
     Unlike electrical and computer engineers, computer scientists deal mostly
     with software and software systems; this includes their theory, design,
     development, and application. Principal areas of study within Computer
@@ -16,11 +20,12 @@ sample_text = """
     scientists encounter range from the abstract-- determining what problems
     can be solved with computers and the complexity of the algorithms that
     solve them – to the tangible – designing applications that perform well
-    on handheld devices, that are easy to use, and that uphold security measures.
-"""
+    on handheld devices, that are easy to use, and that uphold security measures."""
+
 text = st.text_area(label="Enter text", value=sample_text)
+
 btnResult = st.button('Extract Keywords')
 if btnResult:
-    kw_model = KeyBERT('paraphrase-multilingual-mpnet-base-v2')
+    kw_model = KeyBERT(chosen_model)
     keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 2),use_maxsum=True, nr_candidates=20, top_n=5)
     st.write(keywords)
