0e8f4e0b2ee8d31a58f4ccbd76a4fab92c700e08
PakDanan <danan_p@yahoo.com>
2023-08-12 11:33:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,9 @@ chosen_model = st.selectbox(
     'Choose model',
     ('all-MiniLM-L6-v2', 'paraphrase-multilingual-MiniLM-L12-v2', 'paraphrase-mpnet-base-v2','paraphrase-multilingual-mpnet-base-v2'))
 
-sample_text = 
-    """
-    Computer Science is the study of computers and computational systems.
-    Unlike electrical and computer engineers, computer scientists deal mostly
-    with software and software systems; this includes their theory, design,
-    development, and application. Principal areas of study within Computer
-    Science include artificial intelligence, computer systems and networks,
-    security, database systems, human computer interaction, vision and graphics,
-    numerical analysis, programming languages, software engineering, bioinformatics
-    and theory of computing. Although knowing how to program is essential to
-    the study of computer science, it is only one element of the field. Computer
-    scientists design and analyze algorithms to solve programs and study the
-    performance of computer hardware and software. The problems that computer
-    scientists encounter range from the abstract-- determining what problems
-    can be solved with computers and the complexity of the algorithms that
-    solve them – to the tangible – designing applications that perform well
-    on handheld devices, that are easy to use, and that uphold security measures.
-    """
+sample_text = """
+Changes in carbon dioxide levels in the atmosphere in the past played a key role in determining the timing and location of early human species mating. Neanderthal and Denisovan species that had different environmental preferences were able to meet, interbreed, and produce mixed offspring.
+"""
 
 text = st.text_area(label="Enter text", value=sample_text)
 
