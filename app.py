86cdf6389e743e2513d22cf972dbdebff7d3237f
PakDanan <danan_p@yahoo.com>
2023-08-12 11:55:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ chosen_model = st.selectbox(
     ('all-MiniLM-L6-v2', 'paraphrase-multilingual-MiniLM-L12-v2', 'paraphrase-mpnet-base-v2','paraphrase-multilingual-mpnet-base-v2'))
 
 top_n = st.slider('Number of keywords', min_value=5, max_value=20, step=1)
+ngram_range = st.slider('ngram range',min_value=1, max_value=3, (1, 2))
 
 sample_text = """Changes in carbon dioxide levels in the atmosphere in the past played a key role in determining the timing and location of early human species mating. Neanderthal and Denisovan species that had different environmental preferences were able to meet, interbreed, and produce mixed offspring.
 This was told by the international team in the journal Science which was published on August 10, 2023. This research responded to the results of research in 2018. At that time a number of researchers announced the discovery of a female individual -nicknamed Denny-who lived 90,000 years ago."""
@@ -15,5 +16,5 @@ text = st.text_area(label="Enter text", value=sample_text)
 btnResult = st.button('Extract Keywords')
 if btnResult:
     kw_model = KeyBERT(chosen_model)
-    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 2),use_maxsum=True, nr_candidates=20, top_n=top_n)
+    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=ngram_range,use_maxsum=True, nr_candidates=20, top_n=top_n)
     st.write(keywords)
