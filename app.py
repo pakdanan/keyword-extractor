99c45fd575dc6575e0cb56fad61f65359e411138
PakDanan <danan_p@yahoo.com>
2023-08-12 11:50:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from keybert import KeyBERT
 
 chosen_model = st.selectbox(
-    'Choose model',
+    'Model',
     ('all-MiniLM-L6-v2', 'paraphrase-multilingual-MiniLM-L12-v2', 'paraphrase-mpnet-base-v2','paraphrase-multilingual-mpnet-base-v2'))
 
-top_n = st.slider('Select Top ..', min_value=5, max_value=20, step=1)
+top_n = st.slider('Number of keywords', min_value=5, max_value=20, step=1)
 
 sample_text = """Changes in carbon dioxide levels in the atmosphere in the past played a key role in determining the timing and location of early human species mating. Neanderthal and Denisovan species that had different environmental preferences were able to meet, interbreed, and produce mixed offspring.
 This was told by the international team in the journal Science which was published on August 10, 2023. This research responded to the results of research in 2018. At that time a number of researchers announced the discovery of a female individual -nicknamed Denny-who lived 90,000 years ago."""
